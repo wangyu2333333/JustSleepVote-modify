0c48129e0a5e63d721744714f3e9aef4addb800f
Krxwallo <krxwallo@users.noreply.github.com>
2021-01-03 19:15:01 +0100

    Version 1.1

--- src/main/java/com/justAm0dd3r/jsv/JustSleepVote.java
+++ src/main/java/com/justAm0dd3r/jsv/JustSleepVote.java
@@ -12,7 +12,7 @@ import java.io.File;
 public class JustSleepVote
 {
     public static final String NAME = "JustSleepVote";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     public static final String MODID = "jsv";
 
     // Config instance
